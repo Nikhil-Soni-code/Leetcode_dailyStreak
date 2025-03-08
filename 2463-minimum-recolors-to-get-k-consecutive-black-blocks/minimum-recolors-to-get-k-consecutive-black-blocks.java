e6daacb1b2da3a261fa6f907ef100c1088cbd51d
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-03-08 19:40:20 +0530

    Time: 1 ms (92.10%) | Memory: 41.8 MB (26.70%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,21 +1,47 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int minOperations = 101;
-        for(int i=0;i<blocks.length();i++){
-            int whiteCount=0;
-            int blackCount=0;
-            for(int j=i;j<blocks.length();j++){
-                if(blocks.charAt(j)=='W'){
-                    whiteCount++;
-                }else{
-                    blackCount++;
-                }
-                if(whiteCount+blackCount==k){
-                    minOperations = Math.min(minOperations,whiteCount);
-                    break;
-                }
+        int whiteCount = 0;
+        int i=0;
+        for(i=0;i<k;i++){
+            if(blocks.charAt(i)=='W'){
+                whiteCount++;
             }
         }
-        return minOperations;
+        int j=0;
+        int minimumOpr = whiteCount;
+        while(i<blocks.length()){
+            if(blocks.charAt(j)=='W'){
+                whiteCount--;
+            }
+            j++;
+            if(blocks.charAt(i)=='W'){
+                whiteCount++;
+            }
+            i++;
+            minimumOpr = Math.min(minimumOpr,whiteCount);
+        }
+        return minimumOpr;
+
     }
 }
+// class Solution {
+//     public int minimumRecolors(String blocks, int k) {
+//         int minOperations = 101;
+//         for(int i=0;i<blocks.length();i++){
+//             int whiteCount=0;
+//             int blackCount=0;
+//             for(int j=i;j<blocks.length();j++){
+//                 if(blocks.charAt(j)=='W'){
+//                     whiteCount++;
+//                 }else{
+//                     blackCount++;
+//                 }
+//                 if(whiteCount+blackCount==k){
+//                     minOperations = Math.min(minOperations,whiteCount);
+//                     break;
+//                 }
+//             }
+//         }
+//         return minOperations;
+//     }
+// }
