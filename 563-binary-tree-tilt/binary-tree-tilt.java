9a29a747a196aa52d3b8783e81a99637c04ebdd4
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-03-31 09:57:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (73.47%) - LeetSync

---
+++ 563-binary-tree-tilt/binary-tree-tilt.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    private int find(TreeNode root){
+        if(root==null)return 0;
+        int left = find(root.left);
+        int right = find(root.right);
+
+        sum+=Math.abs(left-right);
+        return root.val+left+right;
+    }
+    public int findTilt(TreeNode root) {
+        find(root);
+        return sum;
+    }
+}
