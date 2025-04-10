55da6c803e983627103b8ec3e0a63cb9c00fdf6f
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-04-10 11:12:57 +0530

    Time: 14 ms (59.72%) | Memory: 45.5 MB (95.22%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -13,9 +13,13 @@ class Solution {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
         int[] ans = new int[k];
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> b.count-a.count);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> a.count-b.count);
         for(Integer key:map.keySet()){
+
             pq.add(new Pair(key,map.get(key)));
+                        if(pq.size()>k){
+                pq.poll();
+            }
         }
         for(int i=0;i<k;i++){
             ans[i] = pq.poll().val;
