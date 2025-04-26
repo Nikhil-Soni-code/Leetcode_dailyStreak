009189cba382d73d7c63d60afabeced92b69912a
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-04-26 21:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (22.35%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,36 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     private int findLength(ListNode head){
         int length = 0;
-        while(head != null){
+        while(head!=null){
             head = head.next;
             length++;
         }
         return length;
     }
-
     public ListNode reverseKGroup(ListNode head, int k) {
         int length = findLength(head);
-        if(head == null || head.next == null || length < k) return head;
-
+        if(head==null||head.next==null||length<k)return head;
+        int i=1;
         ListNode prev = null;
         ListNode curr = head;
         ListNode next = curr.next;
-        int i = 1;
-
-        while(i < k){
+        while(i<k){
             curr.next = prev;
             prev = curr;
             curr = next;
-            if(next != null){
+            if(next!=null){
                 next = next.next;
             }
             i++;
         }
-        curr.next = prev; // Reverse the last node of the group also
+        curr.next = prev;
+        head.next = reverseKGroup(next,k);
+        return curr;
 
-        // Now head has become the last node of the reversed group
-        head.next = reverseKGroup(next, k);
-
-        return curr; // Return new head (which is original kth node)
     }
 }
