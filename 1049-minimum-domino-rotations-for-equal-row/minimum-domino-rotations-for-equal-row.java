cefdf8e1dcbc11e429689642d570c04e8daae46d
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-05-03 10:14:10 +0530

    Time: 10 ms (26.40%) | Memory: 50.4 MB (64.53%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
@@ -1,53 +1,54 @@
 class Solution {
-    private int makingTopsSame(int[] tops,int[] bottoms,int rotations,int domino){
-        int ans = Integer.MAX_VALUE;
-        int rot = rotations;
-        boolean flag = true;
-        for(int i=0;i<tops.length;i++){
-            if(tops[i]!=domino&&bottoms[i]!=domino){
-                flag = false;
-            }
-        }return -1;
-    }
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int count1 = 0; //to make everyone tops elements equal to tops[0]
-        int count2 = 0; //to make everyone tops elements equal to bottoms[0]
-        int count3 = 0; //to make everyone bottoms elements equal to tops[0]
-        int count4 = 0; //to make everyone bottoms elements equal to bottoms[0]
-        int domino1 = tops[0];
-        int domino2 = bottoms[0];
+        // We will try to make all values in 'tops' or 'bottoms' equal to either tops[0] or bottoms[0]
+        // and count how many rotations are needed in each scenario.
 
-        for(int i=0;i<tops.length;i++){
-            if(bottoms[i] == domino1&&tops[i]!=domino1){
-                if(count1!=Integer.MAX_VALUE)
-                count1++;
-            }else if(bottoms[i]!=domino1&&tops[i]!=domino1){
+        int count1 = 0; // Count of rotations to make all 'tops' elements equal to tops[0]
+        int count2 = 0; // Count of rotations to make all 'tops' elements equal to bottoms[0]
+        int count3 = 0; // Count of rotations to make all 'bottoms' elements equal to tops[0]
+        int count4 = 0; // Count of rotations to make all 'bottoms' elements equal to bottoms[0]
+
+        int domino1 = tops[0];     // Candidate value from tops
+        int domino2 = bottoms[0];  // Candidate value from bottoms
+
+        for (int i = 0; i < tops.length; i++) {
+
+            // Case 1: Make all tops[i] equal to domino1 (tops[0])
+            // If current top is not domino1 but bottom is, we need a rotation
+            if (bottoms[i] == domino1 && tops[i] != domino1) {
+                if (count1 != Integer.MAX_VALUE) count1++;
+            } 
+            // If neither top nor bottom is domino1, it's not possible
+            else if (bottoms[i] != domino1 && tops[i] != domino1) {
                 count1 = Integer.MAX_VALUE;
             }
 
-            if(bottoms[i] == domino2&&tops[i]!=domino2){
-                if(count2!=Integer.MAX_VALUE)
-                count2++;
-            }else if(bottoms[i]!=domino2&&tops[i]!=domino2){
+            // Case 2: Make all tops[i] equal to domino2 (bottoms[0])
+            if (bottoms[i] == domino2 && tops[i] != domino2) {
+                if (count2 != Integer.MAX_VALUE) count2++;
+            } else if (bottoms[i] != domino2 && tops[i] != domino2) {
                 count2 = Integer.MAX_VALUE;
             }
 
-            if(tops[i] == domino1&&bottoms[i]!=domino1){
-                if(count3!=Integer.MAX_VALUE)
-                count3++;
-            }else if(bottoms[i]!=domino1&&tops[i]!=domino1){
+            // Case 3: Make all bottoms[i] equal to domino1 (tops[0])
+            if (tops[i] == domino1 && bottoms[i] != domino1) {
+                if (count3 != Integer.MAX_VALUE) count3++;
+            } else if (bottoms[i] != domino1 && tops[i] != domino1) {
                 count3 = Integer.MAX_VALUE;
             }
 
-            if(tops[i] == domino2&&bottoms[i]!=domino2){
-                if(count4!=Integer.MAX_VALUE)
-                count4++;
-            }else if(bottoms[i]!=domino2&&tops[i]!=domino2){
+            // Case 4: Make all bottoms[i] equal to domino2 (bottoms[0])
+            if (tops[i] == domino2 && bottoms[i] != domino2) {
+                if (count4 != Integer.MAX_VALUE) count4++;
+            } else if (bottoms[i] != domino2 && tops[i] != domino2) {
                 count4 = Integer.MAX_VALUE;
             }
         }
 
-        int ans = Math.min(count1,Math.min(count2,Math.min(count3,count4)));
-        return (ans == Integer.MAX_VALUE)?-1:ans;
+        // Find the minimum among all 4 valid rotation counts
+        int ans = Math.min(count1, Math.min(count2, Math.min(count3, count4)));
+
+        // If all scenarios failed (i.e., Integer.MAX_VALUE), return -1
+        return (ans == Integer.MAX_VALUE) ? -1 : ans;
     }
 }
