266bc585e014ee658ac48e3740f442054049b7f7
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-05-20 15:50:15 +0530

    Time: 19 ms (14.87%) | Memory: 47.6 MB (36.16%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,40 +1,35 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int[][] copy = new int[mat.length][mat[0].length];
-        int[][] ans = new int[mat.length][mat[0].length];
-
         Queue<int[]> queue = new LinkedList();
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[0].length;j++){
                 copy[i][j] = mat[i][j];
                 if(mat[i][j]==0){
                     queue.add(new int[]{i,j,0});
-                    ans[i][j] = 0;
-                }else{
-                    ans[i][j] = -1;
                 }
             }
         }
-        
+        int[][] ans = new int[mat.length][mat[0].length];
         //bfs
         while(!queue.isEmpty()){
             int[] a = queue.poll();
             int i = a[0];
             int j = a[1];
             int distance = a[2];
-            if(i-1>=0&&copy[i-1][j]==1&&ans[i-1][j]==-1){
+            if(i-1>=0&&copy[i-1][j]==1&&ans[i-1][j]==0){
                 ans[i-1][j] = distance+1;
                 queue.add(new int[]{i-1,j,distance+1});
             }
-            if(j-1>=0&&copy[i][j-1]==1&&ans[i][j-1]==-1){
+                        if(j-1>=0&&copy[i][j-1]==1&&ans[i][j-1]==0){
                 ans[i][j-1] = distance+1;
                 queue.add(new int[]{i,j-1,distance+1});
             }
-            if(i+1<copy.length&&copy[i+1][j]==1&&ans[i+1][j]==-1){
+                        if(i+1<copy.length&&copy[i+1][j]==1&&ans[i+1][j]==0){
                 ans[i+1][j] = distance+1;
                 queue.add(new int[]{i+1,j,distance+1});
             }
-            if(j+1<copy[0].length&&copy[i][j+1]==1&&ans[i][j+1]==-1){
+                        if(j+1<copy[0].length&&copy[i][j+1]==1&&ans[i][j+1]==0){
                 ans[i][j+1] = distance+1;
                 queue.add(new int[]{i,j+1,distance+1});
             }
