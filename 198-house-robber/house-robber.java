2b8045def89c05d7bdd6b6886555aa6c2bd5b237
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-06-26 16:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (60.85%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,44 @@
 class Solution {
-    private int rob(int[] nums,int n,int[] dp){
-        if(n<0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        int notLooted = 0+rob(nums,n-1,dp);
-        int looted = nums[n]+rob(nums,n-2,dp);
-        dp[n] = Math.max(notLooted,looted);
-        return dp[n];
-        
-    }
     public int rob(int[] nums) {
-        int n = nums.length-1;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return rob(nums,n,dp);
+        int n = nums.length;
+        int[]dp = new int[n];
+        if(n==1){
+            return nums[0];
+        }
+        if(n==2){
+            return Math.max(nums[0],nums[1]);
+        }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int max1 = nums[i]+dp[i-2];// take current
+            int max2 = 0+dp[i-1];//leave current
+
+            dp[i] = Math.max(max1,max2);
+        }
+        return dp[n-1];
+
     }
 }
+// class Solution {
+//     // tc-> n
+//     // sc-> n+n
+//     private int rob(int[] nums,int n,int[] dp){
+//         if(n<0)return 0;
+//         if(dp[n]!=-1)return dp[n];
+//         int notLooted = 0+rob(nums,n-1,dp);
+//         int looted = nums[n]+rob(nums,n-2,dp);
+//         dp[n] = Math.max(notLooted,looted);
+//         return dp[n];
+        
+//     }
+//     public int rob(int[] nums) {
+//         int n = nums.length-1;
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return rob(nums,n,dp);
+//     }
+// }
 // class Solution {
 //     //tc -> 2^n
 //     //sc -> n
