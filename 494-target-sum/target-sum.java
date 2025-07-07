aa66054109f1a65bf67a141ed70b22fed42facdf
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-07-07 22:01:49 +0530

    Time: 629 ms (20.19%) | Memory: 41.5 MB (69.29%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,12 +1,15 @@
 class Solution {
-    private int count(int[] nums,int target,int i,int sum){
-        if(i==nums.length){
-            if(sum==target)return 1;
+    private int find(int[] nums,int target,int i){
+        if(i==0){
+            if(target==0)return 1;
             return 0;
         }
-        return count(nums,target,i+1,sum-nums[i])+count(nums,target,i+1,sum+nums[i]);
+        int sub = find(nums,target-nums[i-1],i-1);
+        int add = find(nums,target+nums[i-1],i-1);
+        return sub+add;
+        
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return count(nums,target,0,0);
+        return find(nums,target,nums.length);
     }
 }
