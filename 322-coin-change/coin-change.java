2ec9d784658358ed14130faa29daee7fe94de42e
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-07-07 20:20:00 +0530

    Time: 15 ms (78.24%) | Memory: 45.1 MB (29.84%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,26 +1,52 @@
+// class Solution {
+//     private int minCoin(int[] coins,int i,int amount,int[][] dp){
+//         if(amount==0)return 0;
+//         if(i==0){
+//             if(amount%coins[i]==0) return amount/coins[i];
+//             return MAX;
+//         }
+//         if(dp[i][amount]!=-1)return dp[i][amount];
+//         int notPick = 0+minCoin(coins,i-1,amount,dp);
+//         int pick = MAX;
+//         if(amount>=coins[i]){
+//             pick = 1+minCoin(coins,i,amount-coins[i],dp);
+//         }
+//         dp[i][amount] = Math.min(pick,notPick);
+//         return dp[i][amount];
+//     }
+//     public int coinChange(int[] coins, int amount) {
+//         int[][] dp = new int[coins.length][amount+1];
+//         for(int[] a:dp){
+//             Arrays.fill(a,-1);
+//         }
+//         int ans = minCoin(coins,coins.length-1,amount,dp);
+//         return (ans==MAX)?-1:ans;
+//     }
+// }
 class Solution {
     int MAX = 10000009;
-    private int minCoin(int[] coins,int i,int amount,int[][] dp){
-        if(amount==0)return 0;
-        if(i==0){
-            if(amount%coins[i]==0) return amount/coins[i];
-            return MAX;
-        }
-        if(dp[i][amount]!=-1)return dp[i][amount];
-        int notPick = 0+minCoin(coins,i-1,amount,dp);
-        int pick = MAX;
-        if(amount>=coins[i]){
-            pick = 1+minCoin(coins,i,amount-coins[i],dp);
-        }
-        dp[i][amount] = Math.min(pick,notPick);
-        return dp[i][amount];
-    }
+
     public int coinChange(int[] coins, int amount) {
         int[][] dp = new int[coins.length][amount+1];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
+        for(int amt=0;amt<=amount;amt++){
+            if(amt%coins[0]==0){
+                dp[0][amt] = amt/coins[0];
+            }else{
+                dp[0][amt] = MAX;
+            }
         }
-        int ans = minCoin(coins,coins.length-1,amount,dp);
-        return (ans==MAX)?-1:ans;
+        for(int i=1;i<coins.length;i++){
+            for(int amt = 0;amt<=amount;amt++){
+                int notPick = dp[i-1][amt];
+                int pick = MAX;
+                if(amt>=coins[i]){
+                    pick = 1+dp[i][amt-coins[i]];
+                }
+                dp[i][amt] = Math.min(notPick,pick);
+            }
+        }
+        int ans = dp[coins.length-1][amount];
+        return ans==MAX?-1:ans;
+        
     }
 }
