794894e737969f75a5594458bae5d816575e7f72
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-07-08 12:01:17 +0530

    Time: 7 ms (70.70%) | Memory: 41.8 MB (68.85%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -23,24 +23,44 @@
 //         return count(coins,amount,coins.length-1,dp);
 //     }
 // }
+// class Solution {
+//     public int change(int amount, int[] coins) {
+//         int[][] dp = new int[coins.length][amount+1];
+//         for(int amt = 0;amt <= amount;amt++){
+//             if(amt%coins[0]==0)dp[0][amt] = 1;
+//             else dp[0][amt] = 0;
+//         }
+//         for(int i=1;i<coins.length;i++){
+//             for(int amt=0;amt<=amount;amt++){
+//                 int notPick = dp[i-1][amt];
+//                 int pick = 0;
+//                 if(amt>=coins[i]){
+//                     pick = dp[i][amt-coins[i]];
+//                 }
+//                 dp[i][amt] = notPick+pick;
+//             }
+//         }
+//         return dp[coins.length-1][amount];
+
+//     }
+// }
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length][amount+1];
-        for(int amt = 0;amt <= amount;amt++){
-            if(amt%coins[0]==0)dp[0][amt] = 1;
-            else dp[0][amt] = 0;
+        int[] dp = new int[amount+1];
+        for(int amt=0;amt<=amount;amt++){
+            if(amt%coins[0]==0)dp[amt] = 1;
+            else dp[amt] = 0;
         }
         for(int i=1;i<coins.length;i++){
             for(int amt=0;amt<=amount;amt++){
-                int notPick = dp[i-1][amt];
+                int notPick = dp[amt];
                 int pick = 0;
                 if(amt>=coins[i]){
-                    pick = dp[i][amt-coins[i]];
+                    pick = dp[amt-coins[i]];
                 }
-                dp[i][amt] = notPick+pick;
+                dp[amt] = notPick+pick;
             }
         }
-        return dp[coins.length-1][amount];
-
+        return dp[amount];
     }
 }
