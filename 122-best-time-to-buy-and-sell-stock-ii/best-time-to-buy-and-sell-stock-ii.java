488d4af80686d0f99dce9ebef0e40505f0c49c6e
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-07-29 06:11:55 +0530

    Time: 1 ms (78.00%) | Memory: 45.9 MB (59.34%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,29 +1,50 @@
-class Solution {
-    private int findMax(int[] prices,int previouslyBought,int i,int[][]dp){
-        if(i==prices.length){
-            return 0;
+// class Solution {
+//     private int findMax(int[] prices,int previouslyBought,int i,int[][]dp){
+//         if(i==prices.length){
+//             return 0;
 
-        }
-        if(dp[i][previouslyBought]!=-1)return dp[i][previouslyBought];
-        int max = 0;
-        if(previouslyBought==1){
-            int max1 = prices[i]+findMax(prices,0,i+1,dp);
-            int max2 = findMax(prices,1,i+1,dp);
-            dp[i][previouslyBought] = Math.max(max1,max2);
-            return Math.max(max1,max2);
-        }
-        else{
-            int max1 = -prices[i]+findMax(prices,1,i+1,dp);
-            int max2 = findMax(prices,0,i+1,dp);
-            dp[i][previouslyBought] = Math.max(max1,max2);
-            return Math.max(max1,max2);
-        }
-    }
+//         }
+//         if(dp[i][previouslyBought]!=-1)return dp[i][previouslyBought];
+//         int max = 0;
+//         if(previouslyBought==1){
+//             int max1 = prices[i]+findMax(prices,0,i+1,dp);
+//             int max2 = findMax(prices,1,i+1,dp);
+//             dp[i][previouslyBought] = Math.max(max1,max2);
+//             return Math.max(max1,max2);
+//         }
+//         else{
+//             int max1 = -prices[i]+findMax(prices,1,i+1,dp);
+//             int max2 = findMax(prices,0,i+1,dp);
+//             dp[i][previouslyBought] = Math.max(max1,max2);
+//             return Math.max(max1,max2);
+//         }
+//     }
+//     public int maxProfit(int[] prices) {
+//         int[][] dp = new int[prices.length+1][2];
+//         for(int[]a:dp){
+//             Arrays.fill(a,-1);
+//         }
+//         return findMax(prices,0,0,dp);
+//     }
+// }
+class Solution {
     public int maxProfit(int[] prices) {
         int[][] dp = new int[prices.length+1][2];
-        for(int[]a:dp){
-            Arrays.fill(a,-1);
+        for(int i=prices.length-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(j==1){
+                    int max1 = prices[i]+dp[i+1][0];
+                    int max2 = dp[i+1][1];
+                    dp[i][j] = Math.max(max1,max2);
+                }
+                else{
+                    int max1 = -prices[i]+dp[i+1][1];
+                    int max2 = dp[i+1][0];
+                    dp[i][j] = Math.max(max1,max2);
+                }
+            }
         }
-        return findMax(prices,0,0,dp);
+        return dp[0][0];
+
     }
 }
