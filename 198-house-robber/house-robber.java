4c8ee512adf46b707a37d91cb93e1b32c1328cfa
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-08-07 20:22:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (15.02%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,94 +1,19 @@
 class Solution {
-    // tc-> N
-    // sc-> 1
+    private int find(int[] nums,int i,int[] dp){
+        if(i==0){
+            return nums[i];
+        }
+        if(i<0)return 0;
+        if(dp[i]!=-1)return dp[i];
+
+        int pick = nums[i]+find(nums,i-2,dp);
+        int notPick = 0+find(nums,i-1,dp);
+        dp[i] = Math.max(pick,notPick);
+        return Math.max(pick,notPick);
+    }
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n==1){
-            return nums[0];
-        }
-        int prevprev = nums[0];
-        int prev = Math.max(prevprev,nums[1]);
-
-        for(int i=2;i<n;i++){
-            int notLooted = 0+prev;
-            int looted = nums[i]+prevprev;
-
-            prevprev = prev;
-            prev = Math.max(notLooted,looted);
-        }
-        return prev;
+        int[] dp = new int[nums.length+1];
+        Arrays.fill(dp,-1);
+        return find(nums,nums.length-1,dp);
     }
 }
-
-// __________________________________________________________________________
-
-
-//     // tc-> N
-//     // sc-> N
-// class Solution {
-
-//     public int rob(int[] nums) {
-//         int n = nums.length;
-//         int[]dp = new int[n];
-//         if(n==1){
-//             return nums[0];
-//         }
-//         if(n==2){
-//             return Math.max(nums[0],nums[1]);
-//         }
-//         dp[0] = nums[0];
-//         dp[1] = Math.max(nums[0],nums[1]);
-//         for(int i=2;i<n;i++){
-//             int max1 = nums[i]+dp[i-2];// take current
-//             int max2 = 0+dp[i-1];//leave current
-
-//             dp[i] = Math.max(max1,max2);
-//         }
-//         return dp[n-1];
-
-//     }
-// }
-
-// __________________________________________________________________________
-
-
-//     // tc-> n
-//     // sc-> n+n
-// class Solution {
-
-//     private int rob(int[] nums,int n,int[] dp){
-//         if(n<0)return 0;
-//         if(dp[n]!=-1)return dp[n];
-//         int notLooted = 0+rob(nums,n-1,dp);
-//         int looted = nums[n]+rob(nums,n-2,dp);
-//         dp[n] = Math.max(notLooted,looted);
-//         return dp[n];
-        
-//     }
-//     public int rob(int[] nums) {
-//         int n = nums.length-1;
-//         int[] dp = new int[n+1];
-//         Arrays.fill(dp,-1);
-//         return rob(nums,n,dp);
-//     }
-// }
-
-// __________________________________________________________________________
-
-//     //tc -> 2^n
-//     //sc -> n
-// class Solution {
-
-//     private int rob(int[] nums,int n){
-//         if(n<0)return 0;
-//         int notLooted = 0+rob(nums,n-1);
-//         int looted = nums[n]+rob(nums,n-2);
-
-//         return Math.max(notLooted,looted);
-        
-//     }
-//     public int rob(int[] nums) {
-//         int n = nums.length-1;
-//         return rob(nums,n);
-//     }
-// }
