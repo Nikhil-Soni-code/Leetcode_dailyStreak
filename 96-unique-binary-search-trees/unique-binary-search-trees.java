b5c76e8f0a5933be8fb6a316dd10366e50807d24
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-08-22 11:07:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (37.66%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,10 +1,19 @@
 class Solution {
-    public int numTrees(int n) {
+    private int find(int n,int[] dp){
         if(n==0)return 1;
         if(n==1||n==2)return n;
+        if(dp[n]!=-1)return dp[n];
         int ans = 0;
         for(int i=1;i<=n;i++){
-            ans+=numTrees(i-1)*numTrees(n-i);
-        }return ans;
+            ans+=find(i-1,dp)*find(n-i,dp);
+
+        }
+        dp[n] = ans;
+        return ans;
+    }
+    public int numTrees(int n) {
+        int[]dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return find(n,dp);
     }
 }
