e795c4d4aea3d5c64561b503d6bf42d7c9314160
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-09-04 23:26:12 +0530

    Time: 143 ms (54.51%) | Memory: 41.6 MB (62.53%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,34 +1,21 @@
 class Solution {
-    private boolean search(char[][] board,String word,int idx,int i,int j){
-        if(idx==word.length()){
-            return true;
-        }
-        if(i>=board.length||j>=board[0].length||i<0||j<0||board[i][j]=='*'||board[i][j]!=word.charAt(idx)){
-            return false;
-        }
+    private boolean check(char[][] board,String word,int i,int row,int col){
+        if(i==word.length())return true;
+        if(row<0||col<0||row==board.length||col==board[0].length||board[row][col]=='*')return false;
+        if(word.charAt(i)!=board[row][col])return false;
+        board[row][col] = '*';
+        boolean result = check(board,word,i+1,row+1,col)||check(board,word,i+1,row-1,col)||check(board,word,i+1,row,col+1)||check(board,word,i+1,row,col-1);
+        board[row][col] = word.charAt(i);
 
-        char temp = board[i][j];
-        board[i][j] = '*';
-        if(search(board,word,idx+1,i-1,j))return true;
-        if(search(board,word,idx+1,i,j-1))return true;
-        if(search(board,word,idx+1,i+1,j))return true;
-        if(search(board,word,idx+1,i,j+1))return true;
-        board[i][j] = temp;
-        return false;
-        
-
-
+        return result;
     }
     public boolean exist(char[][] board, String word) {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(search(board,word,0,i,j)){
-                        return true;
-                    }
+                    if(check(board,word,0,i,j))return true;
                 }
             }
-        }
-        return false;
+        }return false;
     }
 }
