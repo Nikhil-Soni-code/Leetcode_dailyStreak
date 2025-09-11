c7e7aa81da5b77a1db63e2eac7952a137f3574dc
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-09-11 23:05:20 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (51.06%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,34 +1,19 @@
 class Solution {
-    private void dfs(int i,int j,int color,int[][]image,int[][]ans,int prevColor){
-        if(i<0||i>=image.length||j<0||j>=image[0].length)return;
-        if(i-1>=0&&image[i-1][j]==prevColor&&ans[i-1][j]==prevColor){
-            ans[i-1][j] = color;
-            dfs(i-1,j,color,image,ans,prevColor);
+    private void ff(int[][] image,int i,int j,int color,int target){
+        if(i==image.length||j==image[0].length||i<0||j<0||image[i][j]!=target){
+            return;
         }
-        if(j-1>=0&&image[i][j-1]==prevColor&&ans[i][j-1]==prevColor){
-            ans[i][j-1] = color;
-            dfs(i,j-1,color,image,ans,prevColor);
-        }
-        if(i+1<image.length&&image[i+1][j]==prevColor&&ans[i+1][j]==prevColor){
-            ans[i+1][j] = color;
-            dfs(i+1,j,color,image,ans,prevColor);
-        }
-        if(j+1<image[0].length&&image[i][j+1]==prevColor&&ans[i][j+1]==prevColor){
-            ans[i][j+1] = color;
-            dfs(i,j+1,color,image,ans,prevColor);
-        }
+        image[i][j] = color;
+        ff(image,i-1,j,color,target);
+        ff(image,i+1,j,color,target);
+        ff(image,i,j-1,color,target);
+        ff(image,i,j+1,color,target);
+
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int[][] ans = new int[image.length][image[0].length];
-        for(int i=0;i<image.length;i++){
-            for(int j=0;j<image[0].length;j++){
-                ans[i][j] = image[i][j];
-            }
-        }
-        if (image[sr][sc] != color) {
-            ans[sr][sc] = color;  // fix: paint starting point
-            dfs(sr, sc, color, image, ans, image[sr][sc]);
-        }
-        return ans;
+        int target = image[sr][sc];
+        if(target==color)return image;
+        ff(image,sr,sc,color,target);
+        return image;
     }
 }
