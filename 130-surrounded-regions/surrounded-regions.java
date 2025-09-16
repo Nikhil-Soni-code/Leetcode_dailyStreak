27f2559d8c69c30ebe98909c7f3f514aa1827fe5
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-09-16 23:27:42 +0530

    Time: 2 ms (83.26%) | Memory: 45.6 MB (35.92%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,51 +1,42 @@
 class Solution {
-    private void dfs(int i,int j,char[][]board){
-        if(i<0||j<0||i==board.length||j==board[0].length||board[i][j]!='O'){
-            return;
-        }
+    private void dfs(char[][] board,int i,int j){
+        if(i<0||j<0||i==board.length||j==board[0].length||board[i][j]=='X'||board[i][j]=='!')return;
         board[i][j] = '!';
-        dfs(i-1,j,board);
-        dfs(i+1,j,board);
-        dfs(i,j-1,board);
-        dfs(i,j+1,board);
-
+        dfs(board,i-1,j);
+        dfs(board,i+1,j);
+        dfs(board,i,j-1);
+        dfs(board,i,j+1);
     }
     public void solve(char[][] board) {
-        int i1=0;
-        int i2=board.length-1;
-
-        int j1=0;
-        int j2=0;
-        for(j1=0;j1<board[0].length;j1++){
-            if(board[i1][j1]=='O'){
-                dfs(i1,j1,board);
-            }
-            if(board[i2][j1]=='O'){
-                dfs(i2,j1,board);
+        for(int i=0;i<board.length;i++){
+            if(board[i][0]=='O'){
+                dfs(board,i,0);
             }
         }
-        i1=0;
-        j1=0;
-        j2=board[0].length-1;
-        for(i1=0;i1<board.length;i1++){
-            if(board[i1][j1]=='O'){
-                dfs(i1,j1,board);
+        for(int i=0;i<board.length;i++){
+            if(board[i][board[0].length-1]=='O'){
+                dfs(board,i,board[0].length-1);
             }
-            if(board[i1][j2]=='O'){
-                dfs(i1,j2,board);
+        }
+        for(int j=0;j<board[0].length;j++){
+            if(board[0][j]=='O'){
+                dfs(board,0,j);
+            }
+        }
+        for(int j=0;j<board[0].length;j++){
+            if(board[board.length-1][j]=='O'){
+                dfs(board,board.length-1,j);
             }
         }
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='O'){
-                    board[i][j] = 'X';
+                if(board[i][j]=='!'){
+                    board[i][j] = 'O';
                 }
-                else if(board[i][j]=='!'){
-                    board[i][j]='O';
+                else if(board[i][j]=='O'){
+                    board[i][j] = 'X';
                 }
             }
         }
-        return;
-
     }
 }
