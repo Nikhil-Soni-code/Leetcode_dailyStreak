6b3df331a65072f849edfa72e80ca529544da5ec
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-09-16 15:46:54 +0530

    Time: 14 ms (56.84%) | Memory: 47.3 MB (65.18%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,40 +1,52 @@
+class Pair{
+    int i;
+    int j;
+    int dis;
+    public Pair(int i,int j,int dis){
+        this.i = i;
+        this.j = j;
+        this.dis = dis;
+    }
+}
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int[][] copy = new int[mat.length][mat[0].length];
-        Queue<int[]> queue = new LinkedList();
+        int[][] dist = new int[mat.length][mat[0].length];
+        Queue<Pair> queue = new LinkedList();
+        for(int[]a:dist){
+            Arrays.fill(a,-1);
+        }
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[0].length;j++){
-                copy[i][j] = mat[i][j];
                 if(mat[i][j]==0){
-                    queue.add(new int[]{i,j,0});
+                    queue.add(new Pair(i,j,0));
+                    dist[i][j] = 0;
                 }
             }
         }
-        int[][] ans = new int[mat.length][mat[0].length];
-        //bfs
         while(!queue.isEmpty()){
-            int[] a = queue.poll();
-            int i = a[0];
-            int j = a[1];
-            int distance = a[2];
-            if(i-1>=0&&copy[i-1][j]==1&&ans[i-1][j]==0){
-                ans[i-1][j] = distance+1;
-                queue.add(new int[]{i-1,j,distance+1});
+            Pair pair = queue.poll();
+            int i = pair.i;
+            int j = pair.j;
+            int dis = pair.dis;
+            if(i+1<mat.length&&dist[i+1][j]==-1){
+                queue.add(new Pair(i+1,j,dis+1));
+                dist[i+1][j] = dis+1;
             }
-                        if(j-1>=0&&copy[i][j-1]==1&&ans[i][j-1]==0){
-                ans[i][j-1] = distance+1;
-                queue.add(new int[]{i,j-1,distance+1});
+            if(j+1<mat[0].length&&dist[i][j+1]==-1){
+                queue.add(new Pair(i,j+1,dis+1));
+                dist[i][j+1] = dis+1;
             }
-                        if(i+1<copy.length&&copy[i+1][j]==1&&ans[i+1][j]==0){
-                ans[i+1][j] = distance+1;
-                queue.add(new int[]{i+1,j,distance+1});
+            if(i-1>=0&&dist[i-1][j]==-1){
+                queue.add(new Pair(i-1,j,dis+1));
+                dist[i-1][j] = dis+1;
             }
-                        if(j+1<copy[0].length&&copy[i][j+1]==1&&ans[i][j+1]==0){
-                ans[i][j+1] = distance+1;
-                queue.add(new int[]{i,j+1,distance+1});
+            if(j-1>=0&&dist[i][j-1]==-1){
+                queue.add(new Pair(i,j-1,dis+1));
+                dist[i][j-1] = dis+1;
             }
         }
-        return ans;
-         
+        return dist;
+
+        
     }
 }
