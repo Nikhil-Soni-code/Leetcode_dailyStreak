62c5a7211739735aa40d62e1c47cd11773caf3e3
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-09-18 00:44:05 +0530

    Time: 7 ms (86.18%) | Memory: 56.4 MB (57.53%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,52 +1,41 @@
 class Solution {
-    private void dfs(int i,int j,int[][] grid){
-        if(i>=grid.length||j>=grid[0].length||i<0||j<0||grid[i][j] == 0){
-            return;
-        }
+    private void dfs(int[][] grid,int i,int j){
+        if(i<0||j<0||i==grid.length||j==grid[0].length||grid[i][j]==0)return;
         grid[i][j] = 0;
-        dfs(i-1,j,grid);
-        dfs(i+1,j,grid);
-        dfs(i,j+1,grid);
-        dfs(i,j-1,grid);
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+
+
 
     }
     public int numEnclaves(int[][] grid) {
-        int[][] copy = new int[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            if(grid[i][0]==1){
+                dfs(grid,i,0);
+            }
+        }
+        for(int i=0;i<grid.length;i++){
+            if(grid[i][grid[0].length-1]==1){
+                dfs(grid,i,grid[0].length-1);
+            }
+        }
+        for(int j=0;j<grid[0].length;j++){
+            if(grid[0][j]==1){
+                dfs(grid,0,j);
+            }
+        }
+        for(int j=0;j<grid[0].length;j++){
+            if(grid[grid.length-1][j]==1){
+                dfs(grid,grid.length-1,j);
+            }
+        }
+        int count = 0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                copy[i][j] = grid[i][j];
+                if(grid[i][j]==1)count++;
             }
-        }
-        for(int i=0;i<grid.length;i++){
-            if(copy[i][0]==1){
-                dfs(i,0,copy);
-            }
-        }
-        for(int i=0;i<grid[0].length;i++){
-            if(copy[0][i]==1){
-                dfs(0,i,copy);
-            }
-        }
-        for(int i=0;i<grid.length;i++){
-            if(copy[i][copy[0].length-1]==1){
-                dfs(i,copy[0].length-1,copy);
-            }
-        }
-        for(int i=0;i<grid[0].length;i++){
-            if(copy[grid.length-1][i]==1){
-                dfs(grid.length-1,i,copy);
-            }
-        }
-
-        int count= 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(copy[i][j]==1){
-                    count++;
-                }
-                
-            }
-        }
-        return count;
+        }return count;
     }
 }
