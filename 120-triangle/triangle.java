727272e5bcf000a2925ac45e2e1e2ad2a59fd01d
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-09-25 11:51:45 +0530

    Time: 1 ms (99.85%) | Memory: 45.9 MB (12.73%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,39 +1,44 @@
-class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int[] dp = new int[triangle.get(triangle.size()-1).size()];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0] = triangle.get(0).get(0);
-        for(int i=1;i<triangle.size();i++){
-            for(int j=triangle.get(i).size()-1;j>=0;j--){
-                int x=dp[j],y=Integer.MAX_VALUE;
-                if(j>0)y = dp[j-1];
-                dp[j] = Math.min(x,y)+triangle.get(i).get(j);
-            }
-        }
-                int min = Integer.MAX_VALUE;
-        for (int val : dp) {
-            min = Math.min(min, val);
-        }
-        return min;
-    }
-}
 // class Solution {
-//     private int find(List<List<Integer>> triangle,int i,int row,int[][] dp){
-//         if(row==triangle.size())return 0;
-//         if(dp[row][i]!=-1)return dp[row][i];
-//         int pick_i = find(triangle,i,row+1,dp);
-//         int pick_nexti = find(triangle,i+1,row+1,dp);
-//         dp[row][i] = triangle.get(row).get(i)+Math.min(pick_i,pick_nexti);
-//         return dp[row][i];
-
-//     }
 //     public int minimumTotal(List<List<Integer>> triangle) {
-//         int[][]dp = new int[triangle.size()][];
-//         for(int i=0;i<dp.length;i++){
-//             dp[i] = new int[triangle.get(i).size()];
-//             Arrays.fill(dp[i],-1);
+//         int[] dp = new int[triangle.get(triangle.size()-1).size()];
+//         Arrays.fill(dp,Integer.MAX_VALUE);
+//         dp[0] = triangle.get(0).get(0);
+//         for(int i=1;i<triangle.size();i++){
+//             for(int j=triangle.get(i).size()-1;j>=0;j--){
+//                 int x=dp[j],y=Integer.MAX_VALUE;
+//                 if(j>0)y = dp[j-1];
+//                 dp[j] = Math.min(x,y)+triangle.get(i).get(j);
+//             }
 //         }
-//         return find(triangle,0,0,dp);
-
+//                 int min = Integer.MAX_VALUE;
+//         for (int val : dp) {
+//             min = Math.min(min, val);
+//         }
+//         return min;
 //     }
 // }
+class Solution {
+    private int find(List<List<Integer>> triangle,int row,int col,int[][] dp){
+        if(row==triangle.size()){
+            return 0;
+        }
+        if(dp[row][col]!=Integer.MIN_VALUE)return dp[row][col];
+        int pick = triangle.get(row).get(col)+find(triangle,row+1,col,dp);
+        int pickNext = Integer.MAX_VALUE;
+        if(triangle.get(row).size()>col+1){
+            pickNext = triangle.get(row).get(col+1)+find(triangle,row+1,col+1,dp);
+        }
+        dp[row][col] = Math.min(pick,pickNext);
+        return Math.min(pick,pickNext);
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[][]dp = new int[triangle.size()][];
+        for(int i=0;i<triangle.size();i++){
+            dp[i] = new int[triangle.get(i).size()];
+        }
+        for(int a[]:dp){
+            Arrays.fill(a,Integer.MIN_VALUE);
+        }
+        return find(triangle,0,0,dp);
+    }
+}
