25a7d572eb2e70c8c0893daef988a8c2f9d0ec72
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-10-29 14:01:08 +0530

    Time: 1 ms (35.65%) | Memory: 40.1 MB (94.33%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,17 +1,32 @@
+// class Solution {
+//     private int find(int n,int[] dp){
+//         if(n<=2)return 1;
+//         if(dp[n]!=-1)return dp[n];
+//         int product = 1;
+//         for(int i=1;i<=n-1;i++){
+//             product = Math.max(product,i*Math.max(n-i,find(n-i,dp)));
+//         }
+//         dp[n] = product;
+//         return product;
+//     }
+//     public int integerBreak(int n) {
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return find(n,dp);
+//     }
+// }
 class Solution {
-    private int find(int n,int[] dp){
-        if(n<=2)return 1;
-        if(dp[n]!=-1)return dp[n];
-        int product = 1;
-        for(int i=1;i<=n-1;i++){
-            product = Math.max(product,i*Math.max(n-i,find(n-i,dp)));
-        }
-        dp[n] = product;
-        return product;
-    }
     public int integerBreak(int n) {
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return find(n,dp);
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=3;i<=n;i++){
+            int product = 1;
+            for(int j=1;j<=i-1;j++){
+                product = Math.max(product,j*Math.max(i-j,dp[i-j]));
+            }
+            dp[i] = product;
+        }
+        return dp[n];
     }
 }
