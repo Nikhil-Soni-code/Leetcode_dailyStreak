947f6a6c10874e6c409d43848952211ff1dd6c1d
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-11-03 10:54:22 +0530

    Time: 9 ms (59.83%) | Memory: 93.2 MB (5.68%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -1,20 +1,44 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
+
+        // This will store the total cost of removing balloons
         int minCostRequired = 0;
+
+        // This stores the cost for the current group of same-color balloons
         int cost = 0;
-        for(int i=1;i<colors.length();i++){
-            if(colors.charAt(i)==colors.charAt(i-1)){
-                cost+=Math.min(neededTime[i],neededTime[i-1]);
-                if(neededTime[i]<neededTime[i-1]){
-                    neededTime[i] = neededTime[i-1];
+
+        // Start from index 1 because we compare each balloon with the previous one
+        for(int i = 1; i < colors.length(); i++) {
+
+            // If two adjacent balloons have the same color
+            if(colors.charAt(i) == colors.charAt(i - 1)) {
+
+                // We must remove ONE of them to avoid adjacent duplicates.
+                // Remove the one with the smaller time (cheaper removal).
+                cost += Math.min(neededTime[i], neededTime[i - 1]);
+
+                // Important trick:
+                // We want the "remaining" balloon in this chain to always be the most expensive one.
+                // So if current balloon is cheaper than previous, we replace its time with previous.
+                // This keeps neededTime[i] as the *max* of the chain so far.
+                if(neededTime[i] < neededTime[i - 1]) {
+                    neededTime[i] = neededTime[i - 1];
                 }
-            }else{
-                minCostRequired+=cost;
+
+            } else {
+                // If colors differ, the previous "same-color chain" ends here.
+                // Add its accumulated cost.
+                minCostRequired += cost;
+
+                // Reset for next chain
                 cost = 0;
             }
         }
-        minCostRequired+=cost;
 
+        // Add the cost of the last chain (if any)
+        minCostRequired += cost;
+
+        // Total minimum cost required to remove balloons
         return minCostRequired;
     }
 }
