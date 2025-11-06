e464a21843072b4b4ff280633571200bd9b19d01
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-11-06 13:51:49 +0530

    Time: 3 ms (2.24%) | Memory: 43.8 MB (5.23%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,38 +1,38 @@
 class Solution {
-    private int findPeak(int[] nums){
+    public int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        System.out.print(pivot);
+        int left = binarySearch(nums,target,0,pivot);
+        if(left!=-1)return left;
+        int right = binarySearch(nums,target,pivot+1,nums.length-1);
+        return right;
+    }
+    private int findPivot(int[] nums){
         int beg = 0,end = nums.length-1;
         while(beg<end){
             int mid = beg+(end-beg)/2;
             if(mid+1<nums.length&&nums[mid]>nums[mid+1]){
                 return mid;
+            }if(mid-1>=0&&nums[mid-1]>nums[mid]){
+                return mid-1;
             }
-            if(mid-1>=0&&nums[mid]<nums[mid-1])return mid-1;
-            if(nums[beg]>nums[mid]){
+            else if(nums[beg]>nums[mid]){
                 end = mid-1;
-            }
-            else{
+            }else{
                 beg = mid+1;
             }
         }
         return beg;
     }
-    private int bs(int beg,int[] nums,int target,int end){
+    private int binarySearch(int[] nums,int target,int beg,int end){
         while(beg<=end){
             int mid = beg+(end-beg)/2;
             if(nums[mid]==target)return mid;
-            if(nums[mid]>target){
+            if(target<nums[mid]){
                 end = mid-1;
-            }
-            else{
+            }else{
                 beg = mid+1;
             }
-        }
-        return -1;
-    }
-    public int search(int[] nums, int target) {
-        int peak = findPeak(nums);
-        int found = bs(0,nums,target,peak);
-        if(found!=-1)return found;
-        return bs(peak+1,nums,target,nums.length-1);
+        }return -1;
     }
 }
