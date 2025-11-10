45cd41a0f9dec80673e6896615ea39fe96cd01a6
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-11-10 14:58:29 +0530

    Time: 8 ms (85.85%) | Memory: 48.9 MB (9.48%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,27 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> list = new ArrayList();
+        List<int[]> merged = new ArrayList();
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        list.add(intervals[0]);
-        int idx = 0;
+        int[] interval =  intervals[0];
         for(int i=1;i<intervals.length;i++){
-            if(list.get(idx)[1]>=intervals[i][0]){
-                int[] a = list.get(idx);
-                a[1] = Math.max(intervals[i][1],a[1]);
-                list.remove(idx);
-                list.add(a);
-
-            }
-            else{
-                list.add(intervals[i]);
-                idx++;
+            if(intervals[i][0]<=interval[1]){
+                interval[1] = Math.max(intervals[i][1],interval[1]);
+            }else{
+                merged.add(interval);
+                interval = intervals[i];
             }
         }
-        int[][] ans = new int[list.size()][2];
-        for(int i=0;i<list.size();i++){
-            ans[i] = list.get(i);
-        }
-        return ans;
-
+        merged.add(interval);
+        int[][] ans = new int[merged.size()][2];
+        int idx = 0;
+        for(int[]a:merged){
+            ans[idx++] = a;
+        }return ans;
     }
 }
