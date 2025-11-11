910a077461717db179bb9b9328de20ae875a9d4b
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-11-11 13:06:19 +0530

    Time: 1 ms (29.50%) | Memory: 43.7 MB (6.95%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    private boolean check(String s,int i,int open,Boolean[][] dp){
+        if(i==s.length()){
+            return open==0;
+        }
+        if(open<0)return false;
+        if(dp[i][open]!=null)return dp[i][open];
+        if(s.charAt(i)=='('){
+            dp[i][open] = check(s,i+1,open+1,dp);
+            return dp[i][open];
+        }
+        else if(s.charAt(i)==')'){
+            dp[i][open] = check(s,i+1,open-1,dp);
+            return dp[i][open];
+        }
+        else {
+            dp[i][open] = check(s,i+1,open,dp)||check(s,i+1,open+1,dp)||check(s,i+1,open-1,dp);
+            return dp[i][open];
+        }
+        
+    }
+    public boolean checkValidString(String s) {
+        Boolean[][] dp = new Boolean[s.length()][s.length()];
+        for(Boolean[]a:dp){
+            Arrays.fill(a,null);
+        }
+        return check(s,0,0,dp);
+    }
+}
