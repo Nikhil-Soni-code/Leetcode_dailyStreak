34f74aa65068c9018f077ae15b23132f43b5bbf2
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-11-11 15:56:56 +0530

    Time: 101 ms (9.05%) | Memory: 47.3 MB (5.11%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,32 +1,36 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans = new ArrayList();
-        HashSet<List<Integer>> set = new HashSet();
+        HashSet<List<Integer>> ans = new HashSet();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
+        for(int i=0;i<nums.length-2;i++){
+
+            for(int j=i+1;j<nums.length-1;j++){
                 int k = j+1;
                 int l = nums.length-1;
+                long sum = nums[i]+nums[j];
+
+
+
                 while(k<l){
-                    long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum>target){
-                        l--;
-                    }
-                    else if(sum<target){
-                        k++;
-                    }
-                    else{
-                        List<Integer> subList = new ArrayList();
-                        subList.add(nums[i]);
-                        subList.add(nums[j]);
-                        subList.add(nums[k]);
-                        subList.add(nums[l]);
-                        set.add(new ArrayList(subList));
+                    if(sum+nums[k]+nums[l]==target){
+                List<Integer> subAns = new ArrayList();
+                        subAns.add(nums[i]);
+                        subAns.add(nums[j]);
+                        subAns.add(nums[k]);
+                        subAns.add(nums[l]);
+                        ans.add(new ArrayList(subAns));
                         l--;k++;
                     }
+                    else if(sum+nums[k]+nums[l]>target){
+                        l--;
+                    }else{
+                        k++;
+                    }
                 }
+
             }
         }
-        return new ArrayList(set);
+        return new ArrayList(ans);
+
     }
 }
