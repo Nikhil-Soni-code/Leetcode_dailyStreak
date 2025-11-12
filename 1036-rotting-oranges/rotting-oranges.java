8f2b08b776f4cdc6f2a24ecd86cdb3c64e45f5b8
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-11-12 14:08:56 +0530

    Time: 2 ms (69.07%) | Memory: 43.4 MB (21.23%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,63 +1,64 @@
 class Pair{
-    int idx_1;
-    int idx_2;
-    public Pair(int a,int b){
-        idx_1 = a;
-        idx_2 = b;
+    int i;
+    int j;
+    public Pair(int i,int j){
+        this.i = i;
+        this.j = j;
     }
 }
 class Solution {
-    private int bfs(Queue<Pair> queue,int[][] grid){
-        int minutes = -1;
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i=0;i<size;i++){
-                Pair pair = queue.poll();
-                int a = pair.idx_1;
-                int b = pair.idx_2;
-
-                if(a+1<grid.length&&grid[a+1][b]==1){
-                    grid[a+1][b] = 2;
-                    queue.add(new Pair(a+1,b));
-                }
-                if(b+1<grid[0].length&&grid[a][b+1]==1){
-                    grid[a][b+1] = 2;
-                    queue.add(new Pair(a,b+1));
-                }
-                if(a-1>=0&&grid[a-1][b]==1){
-                    grid[a-1][b] = 2;
-                    queue.add(new Pair(a-1,b));
-                }
-                if(b-1>=0&&grid[a][b-1]==1){
-                    grid[a][b-1] = 2;
-                    queue.add(new Pair(a,b-1));
-                }
-            }
-            minutes++;
-        }
-
-        return minutes;
-    }
-    public int orangesRotting(int[][] grid) {   
-
+    public int orangesRotting(int[][] grid) {
+        boolean freshExist = false;
         Queue<Pair> queue = new LinkedList();
-
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
                     queue.add(new Pair(i,j));
                 }
+                if(grid[i][j]==1){
+                    freshExist = true;
+                }
             }
         }
-
-        int time = bfs(queue,grid);
-                for(int i=0;i<grid.length;i++){
+        if(queue.isEmpty()){
+            if(!freshExist)return 0;
+            return -1;
+            }
+        int time = -1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                Pair pair = queue.poll();
+                int row = pair.i;
+                int col = pair.j;
+                if(row>0&&grid[row-1][col]==1){
+                    grid[row-1][col] = 2;
+                    queue.add(new Pair(row-1,col));
+                }
+                if(row<grid.length-1&&grid[row+1][col]==1){
+                    grid[row+1][col] = 2;
+                    queue.add(new Pair(row+1,col));
+                }
+                if(col>0&&grid[row][col-1]==1){
+                    grid[row][col-1] = 2;
+                    queue.add(new Pair(row,col-1));
+                }
+                if(col<grid[0].length-1 && grid[row][col+1]==1){
+                    grid[row][col+1] = 2;
+                    queue.add(new Pair(row,col+1));
+                }
+            }
+            time++;
+        }
+        for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==1){
                     return -1;
                 }
             }
         }
-        return (time==-1)?0:time;
+        return time;
+        
+
     }
 }
