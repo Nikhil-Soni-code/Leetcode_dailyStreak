f2d0744d33d60efc72bcb062e9a654296b9ec4ba
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-11-20 10:54:24 +0530

    Time: 1011 ms (30.80%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,8 +1,28 @@
 # Write your MySQL query statement below
-SELECT S.student_id,S.student_name,Sub.subject_name,COUNT(E.student_id) AS attended_exams
-FROM Students AS S
+SELECT Stu.student_id,Stu.student_name,Sub.subject_name,IFNULL(COUNT(Exam.student_id), 0) AS attended_exams
+FROM Students AS Stu
 CROSS JOIN Subjects AS Sub
-LEFT JOIN Examinations AS E
-ON S.student_id = E.student_id AND Sub.subject_name = E.subject_name
-GROUP BY Sub.subject_name,S.student_id
-ORDER BY S.student_id, Sub.subject_name;
+LEFT JOIN
+Examinations AS Exam
+ON Stu.student_id = Exam.student_id AND Sub.subject_name = Exam.subject_name
+GROUP BY 
+Sub.subject_name,Stu.student_id
+ORDER BY Stu.student_id,Sub.subject_name;
+
+-- SELECT 
+--     Stu.student_id,
+--     Stu.student_name,
+--     Sub.subject_name,
+--     IFNULL(COUNT(Exam.student_id), 0) AS attended_exams
+-- FROM Students AS Stu
+-- CROSS JOIN Subjects AS Sub
+-- LEFT JOIN Examinations AS Exam
+--     ON Stu.student_id = Exam.student_id 
+--    AND Sub.subject_name = Exam.subject_name
+-- GROUP BY 
+--     Stu.student_id,
+--     Stu.student_name,
+--     Sub.subject_name
+-- ORDER BY 
+--     Stu.student_id,
+--     Sub.subject_name;
