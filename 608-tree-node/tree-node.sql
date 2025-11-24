88c369d0b8f81342dbbe82beb01a69a970294f74
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-11-24 13:30:03 +0530

    Time: 467 ms (39.11%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -2,14 +2,14 @@ SELECT id, 'Root' AS type
 FROM Tree
 WHERE p_id IS NULL
 
-UNION ALL
+UNION
 
 SELECT id, 'Inner' AS type
 FROM Tree
 WHERE p_id IS NOT NULL
   AND id IN (SELECT p_id FROM Tree WHERE p_id is NOT NULL)
 
-UNION ALL
+UNION
 
 SELECT id, 'Leaf' AS type
 FROM Tree
