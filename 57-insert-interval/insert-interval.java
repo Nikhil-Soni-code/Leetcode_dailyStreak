700c81d4b641ad9e4a2240b33c09318192de4da7
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-11-27 23:50:07 +0530

    Time: 1 ms (98.51%) | Memory: 47.1 MB (18.42%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,31 +1,27 @@
 class Solution {
-    private List<int[]> insertInterval(int[][] intervals, int[] newInterval){
-        List<int[]> list = new ArrayList();
-        int i=0;
-        while(i<intervals.length&&intervals[i][1]<newInterval[0]){
-            list.add(intervals[i]);
-            i++;
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> ans = new ArrayList();
+        int i = 0;
+        while(i<intervals.length && intervals[i][1]<newInterval[0]){
+            ans.add(intervals[i++]);
         }
-        while(i<intervals.length&&intervals[i][0]<=newInterval[1]){
+        while(i<intervals.length && intervals[i][0] <= newInterval[1]){
             newInterval[0] = Math.min(newInterval[0],intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1],intervals[i][1]);
             i++;
         }
-        list.add(newInterval);
-        for(int j=i;j<intervals.length;j++){
-            list.add(intervals[j]);
+        ans.add(newInterval);
+
+        while(i<intervals.length){
+            ans.add(intervals[i++]);
         }
-        return list;
+        int[][] a = new int[ans.size()][2];
+        i=0;
+        for(int[] b:ans){
+            a[i++] = b;
+        }
+        return a;
+
 
     }
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list = insertInterval(intervals,newInterval);
-        int[][] ans = new int[list.size()][2];
-        for(int i=0;i<list.size();i++){
-            ans[i] = list.get(i);
-
-        }
-        return ans;
-    }
-
 }
