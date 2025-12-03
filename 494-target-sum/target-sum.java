5687e1689a7d176150b930867a408ff35f96887c
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-12-03 14:50:31 +0530

    Time: 636 ms (12.50%) | Memory: 43.3 MB (53.68%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,37 +1,15 @@
 class Solution {
-    private int count(int[] nums,int i,int target,int[][]dp){
-        if(i==0){
-            if(target==0&&nums[i]==0)return 2;
-            if(target==0||nums[i]==target)return 1;
-
+    private int count(int[] nums,int i,int target, int sum){
+        if(i<0){
+            if(sum==target)return 1;
             return 0;
         }
-        if(dp[i][target+1000]!=-1)return dp[i][target+1000];
-        int notPick = count(nums,i-1,target,dp);
-        int pick = 0;
-        if(target>=nums[i]){
-            pick = count(nums,i-1,target-nums[i],dp);
-        }
-        dp[i][target+1000] = pick+notPick;
-        return dp[i][target+1000];
+        
+        int sub = count(nums,i-1,target,sum-nums[i]);
+        int add = count(nums,i-1,target,sum+nums[i]);
+        return sub+add;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        int sum = 0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
-        }
-    
-        if(sum<target||(sum+target)%2==1)return 0;
-        target = (sum+target)/2;
-        int[][]dp = new int[nums.length][target+1001];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
-        }
-        return count(nums,nums.length-1,target,dp);
+        return count(nums,nums.length-1,target,0);
     }
 }
-// class Solution {
-//     public int findTargetSumWays(int[] nums, int target) {
-        
-//     }
-// }
