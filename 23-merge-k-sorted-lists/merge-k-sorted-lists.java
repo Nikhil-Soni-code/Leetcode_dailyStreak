a79b9be11d1332a1b56ce0d519b41b0f477062f9
Nikhil Soni <nikhilsoni200605@gmail.com>
2025-12-25 23:07:10 +0530

    Time: 4 ms (78.45%) | Memory: 47.2 MB (10.02%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,29 +8,22 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- class Pair{
-    int val;
-    ListNode node;
-    public Pair(int val,ListNode node){
-        this.val = val;
-        this.node = node;
-    }
- }
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
-        for(int i=0;i<lists.length;i++){
-            if(lists[i]!=null)
-            pq.add(new Pair(lists[i].val,lists[i]));
+        if(lists.length==0)return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        for(ListNode node:lists){
+            if(node!=null)pq.add(node);
         }
         ListNode dummyHead = new ListNode(-1);
         ListNode temp = dummyHead;
-        while(pq.size()!=0){
-            Pair pair = pq.poll();
-            temp.next = pair.node;
+        while(!pq.isEmpty()){
+            ListNode node = pq.poll();
+            temp.next = node;
             temp = temp.next;
-            if(pair.node.next!=null){
-                pq.add(new Pair(pair.node.next.val,pair.node.next));
+
+            if(node.next != null){
+                pq.add(node.next);
             }
         }
         return dummyHead.next;
