7d81494ecb71c4dc8a44ce3f98bc9c50cdc04da2
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-01-03 12:28:11 +0530

    Time: 378 ms (10.74%) | Memory: 237.4 MB (7.09%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,22 +1,22 @@
 class Solution {
-    private int find(int[] nums,int i,int prev,int[][] dp){
+    private int findMaxLength(int[] nums,int i,int prev,int[][]dp){
         if(i==nums.length){
             return 0;
         }
-        if(dp[i][prev+1]!=10002)return dp[i][prev+1];
-        int pick = -1;
-        if(prev==-1||nums[i]>nums[prev]){
-            pick = 1+find(nums,i+1,i,dp);
+        if(dp[i][prev+1] != -1)return dp[i][prev+1];
+        int pick = 0;
+        if(prev==-1 || nums[i] > nums[prev]){
+            pick = 1 + findMaxLength(nums,i+1,i,dp);
         }
-        int notPick = find(nums,i+1,prev,dp);
+        int notPick = findMaxLength(nums,i+1,prev,dp);
         dp[i][prev+1] = Math.max(pick,notPick);
         return Math.max(pick,notPick);
     }
     public int lengthOfLIS(int[] nums) {
-        int[][] dp = new int[nums.length][nums.length];
+        int[][] dp = new int[nums.length][nums.length+1];
         for(int[]a:dp){
-            Arrays.fill(a,10002);
+            Arrays.fill(a,-1);
         }
-        return find(nums,0,-1,dp);
+        return findMaxLength(nums,0,-1,dp);
     }
 }
