f40d82bde541f7ddbab299355cad989a87aa6f59
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-01-03 16:07:54 +0530

    Time: 13 ms (11.46%) | Memory: 47.8 MB (13.89%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,28 +1,22 @@
 class Solution {
-    private int find(int i,int[] prices,int previouslyBought,int count,int[][][] dp,int k){
-        if((count==k&&previouslyBought==0)||i==prices.length){
-            return 0;
+    private int buyOrSell(int[] prices,int i,int k,int previouslyBought,int[][][] dp){
+        if(k==0 || i>= prices.length)return 0;
+        if(dp[i][k][previouslyBought] != -1)return dp[i][k][previouslyBought];
+        int buy = 0,sell = 0;
+        if(previouslyBought == 0){
+            buy = -prices[i] + buyOrSell(prices,i+1,k,1,dp);
+        }else{
+            sell = prices[i] + buyOrSell(prices,i+1,k-1,0,dp);
         }
-        if(dp[i][previouslyBought][count]!=-1)return dp[i][previouslyBought][count];
-        if(previouslyBought==1){
-            int sell = prices[i]+find(i+1,prices,0,count,dp,k);
-            int skip = 0+find(i+1,prices,1,count,dp,k);
-            dp[i][previouslyBought][count] = Math.max(sell,skip);
-            return Math.max(sell,skip);
-        }
-        else{
-            int buy = -prices[i]+find(i+1,prices,1,count+1,dp,k);
-            int skip = 0+find(i+1,prices,0,count,dp,k);
-            dp[i][previouslyBought][count] = Math.max(buy,skip);
-            return Math.max(buy,skip);
-        }
+        int hold = buyOrSell(prices,i+1,k,previouslyBought,dp);
+        dp[i][k][previouslyBought] = Math.max(buy,Math.max(sell,hold));
+        return Math.max(buy,Math.max(sell,hold));
     }
     public int maxProfit(int k, int[] prices) {
-        int[][][] dp = new int[prices.length+1][2][k+1];
-        for(int[][]a:dp){
-            for(int[]b:a)
-            Arrays.fill(b,-1);
+        int[][][] dp = new int[prices.length][k+1][2];
+        for(int[][] a:dp){
+            for(int[]b:a)Arrays.fill(b,-1);
         }
-        return find(0,prices,0,0,dp,k);
+        return buyOrSell(prices,0,k,0,dp);
     }
 }
