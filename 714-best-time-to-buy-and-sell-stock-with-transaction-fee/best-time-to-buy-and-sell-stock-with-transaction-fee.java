7e4acdfee4115dcd00a3e6c5171b51a456fa8f42
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-01-03 13:45:09 +0530

    Time: 30 ms (22.94%) | Memory: 77.1 MB (14.47%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,27 +1,26 @@
 class Solution {
-    private int find(int i,int[] prices,int fee,int boughtPreviously,int[][] dp){
-        if(i==prices.length){
-            return 0;
-        }
-        if(dp[i][boughtPreviously]!=-1)return dp[i][boughtPreviously];
-        if(boughtPreviously==1){
-            int sell = prices[i]+find(i+1,prices,fee,0,dp)-fee;
-            int skip = 0+find(i+1,prices,fee,1,dp);
-            dp[i][boughtPreviously] = Math.max(sell,skip);
-            return Math.max(sell,skip);
+    // previouslyBought = 1 yess
+    // previouslyBought = 0 nooo
+
+    private int buyOrSell(int[] prices,int fee,int i,int previouslyBought,int[][] dp){
+        if(i==prices.length)return 0;
+        if(dp[i][previouslyBought] != -1)return dp[i][previouslyBought];
+        int buy = 0,sell = 0;
+        if(previouslyBought==0){
+            buy = -fee - prices[i] + buyOrSell(prices,fee,i+1,1,dp);
         }
         else{
-            int buy = -prices[i]+find(i+1,prices,fee,1,dp);
-            int skip = find(i+1,prices,fee,0,dp);
-            dp[i][boughtPreviously] = Math.max(buy,skip);
-            return Math.max(buy,skip);
+            sell = prices[i] + buyOrSell(prices,fee,i+1,0,dp);
         }
+        int hold = buyOrSell(prices,fee,i+1,previouslyBought,dp);
+        dp[i][previouslyBought] = Math.max(buy,Math.max(sell,hold));
+        return Math.max(buy,Math.max(sell,hold));
     }
     public int maxProfit(int[] prices, int fee) {
-        int[][] dp = new int[prices.length+1][2];
+        int[][] dp = new int[prices.length][2];
         for(int[]a:dp){
             Arrays.fill(a,-1);
         }
-        return find(0,prices,fee,0,dp);
+        return buyOrSell(prices,fee,0,0,dp);
     }
 }
