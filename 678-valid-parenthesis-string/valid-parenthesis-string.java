ce6d32311d12e2e7feaea2f36b8b8b12af10b4b2
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-01-19 10:07:46 +0530

    Time: 2 ms (4.74%) | Memory: 43.9 MB (7.56%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,29 +1,24 @@
 class Solution {
     private boolean check(String s,int i,int open,Boolean[][] dp){
-        if(i==s.length()){
-            return open==0;
-        }
         if(open<0)return false;
+        if(i==s.length())return open==0;
         if(dp[i][open]!=null)return dp[i][open];
         if(s.charAt(i)=='('){
             dp[i][open] = check(s,i+1,open+1,dp);
             return dp[i][open];
         }
         else if(s.charAt(i)==')'){
-            dp[i][open] = check(s,i+1,open-1,dp);
+            dp[i][open] = check(s,i+1,open-+1,dp);
             return dp[i][open];
         }
-        else {
-            dp[i][open] = check(s,i+1,open,dp)||check(s,i+1,open+1,dp)||check(s,i+1,open-1,dp);
+        else{
+            dp[i][open] = check(s,i+1,open+1,dp)||check(s,i+1,open-1,dp)||check(s,i+1,open,dp);
             return dp[i][open];
         }
-        
     }
     public boolean checkValidString(String s) {
         Boolean[][] dp = new Boolean[s.length()][s.length()];
-        for(Boolean[]a:dp){
-            Arrays.fill(a,null);
-        }
+        for(Boolean[] a:dp)Arrays.fill(a,null);
         return check(s,0,0,dp);
     }
 }
