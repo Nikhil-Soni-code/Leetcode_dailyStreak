84067a11553cd9ccbb215935695ed9054ea2f83a
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-01-22 23:51:17 +0530

    Time: 41 ms (27.45%) | Memory: 50.1 MB (33.64%) - LeetSync

---
+++ 923-super-egg-drop/super-egg-drop.java
@@ -0,0 +1,30 @@
+class Solution {
+    private int minNumberOfMoves(int k,int n,int[][]dp){
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if(k==1)return n;
+        if(dp[k][n]!=-1)return dp[k][n];
+        int min = Integer.MAX_VALUE;
+
+        int beg = 1,end = n;
+        while(beg<=end){
+            int mid = beg+(end-beg)/2;
+            int broken = minNumberOfMoves(k-1,mid-1,dp);
+            int unBroken = minNumberOfMoves(k,n-mid,dp);
+            min = Math.min(min,1+Math.max(broken,unBroken));
+            if(broken > unBroken){
+                end = mid-1;
+            }else{
+                beg = mid+1;
+            }
+        }
+        dp[k][n] = min;
+        return min;
+
+    }
+    public int superEggDrop(int k, int n) {
+        int[][] dp = new int[k+1][n+1];
+        for(int[]a:dp)Arrays.fill(a,-1);
+        return minNumberOfMoves(k,n,dp);
+    }
+}
