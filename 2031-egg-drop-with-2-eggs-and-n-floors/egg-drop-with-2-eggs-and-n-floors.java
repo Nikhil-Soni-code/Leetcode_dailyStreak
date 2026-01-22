c6574cea09a9c64974d293511a288c08139f80e5
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-01-23 00:21:14 +0530

    Time: 3 ms (45.61%) | Memory: 42.8 MB (20.08%) - LeetSync

---
+++ 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.java
@@ -0,0 +1,32 @@
+class Solution {
+        private int minNumberOfMoves(int k,int n,int[][]dp){
+        if(n<1)return 0;
+        if(n==1)return 1;
+        if(k==1)return n;
+        if(k==0)return 0;
+
+        if(dp[k][n]!=-1)return dp[k][n];
+        int min = Integer.MAX_VALUE;
+
+        int beg = 1,end = n;
+        while(beg<=end){
+            int mid = beg+(end-beg)/2;
+            int broken = minNumberOfMoves(k-1,mid-1,dp);
+            int unBroken = minNumberOfMoves(k,n-mid,dp);
+            min = Math.min(min,1+Math.max(broken,unBroken));
+            if(broken > unBroken){
+                end = mid-1;
+            }else{
+                beg = mid+1;
+            }
+        }
+        dp[k][n] = min;
+        return min;
+
+    }
+    public int twoEggDrop(int n) {
+        int[][] dp = new int[3][n+1];
+        for(int[]a:dp)Arrays.fill(a,-1);
+        return minNumberOfMoves(2,n,dp);
+    }
+}
