f114a0f9edfcf67e7fe9a96e0d62350ce38d320d
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-01-29 13:32:59 +0530

    Time: 484 ms (9.00%) | Memory: 43.8 MB (34.41%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,29 +1,23 @@
 class Solution {
-    private boolean match(String s, String p, int i, int j) {
-        if (j >= p.length()) {
-            return i == s.length();
+    private boolean match(String s , String p , int i , int j){
+        if(j>=p.length()){
+            return i==s.length();
         }
-
-        if (j < p.length() - 1 && p.charAt(j + 1) == '*'
-                && (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'))) {
-
-            boolean take = match(s, p, i + 1, j);
-            boolean notTake = match(s, p, i, j + 2);
-            return take || notTake;
+        if(i>s.length())return false;
+        if(j<p.length()-1 && ((i==s.length() || s.charAt(i) == p.charAt(j)) || p.charAt(j) == '.')&& p.charAt(j+1) == '*'){
+            boolean take = match(s,p,i+1,j);
+            boolean notTake = match(s,p,i,j+2);
+            return take||notTake;
         }
-
-        if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
-            return match(s, p, i, j + 2);
+        else if(j<p.length()-1 && p.charAt(j+1)=='*')return match(s,p,i,j+2);
+        else if(i<s.length() && s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'){
+            boolean res = match(s,p,i+1,j+1);
+            return res;
         }
-
-        if (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
-            return match(s, p, i + 1, j + 1);
-        }
-
         return false;
-    }
 
+    }
     public boolean isMatch(String s, String p) {
-        return match(s, p, 0, 0);
+        return match(s,p,0,0);
     }
 }
