4d910c3da6d77f86a1e58f25988741bbcaf4f0d5
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-02-16 09:32:19 +0530

    Time: 1 ms (60.34%) | Memory: 42.3 MB (93.42%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int ans = 0;
         int pow = 31;
         while(n!=0){
-            if((n&1)==1)ans += Math.pow(2,pow);
+            if((n&1)==1)ans += (1<<pow);
             pow--;
             n = n>>1;
         }
