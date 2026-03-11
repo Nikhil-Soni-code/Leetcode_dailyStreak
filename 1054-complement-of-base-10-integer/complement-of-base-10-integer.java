3e360d9c3564d2cbb56f9724542a3ccc0865dacf
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-03-11 11:58:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (97.77%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,14 +1,15 @@
 class Solution {
-    public int bitwiseComplement(int num) {
-        if(num==0)return 1;
+    public int bitwiseComplement(int n) {
+        if(n==0)return 1;
         int ans = 0;
-        int i=1;
-        while(num!=0){
-            if((num&1)==0){
-                ans += i;
-            } 
-            i = i<<1;
-            num = num>>1;
-        }return ans;
+        int pow = 1;
+        while(n!=0){
+            if((n&1)==0){
+                ans = ans + pow;
+            }
+            pow = pow*2;
+            n = n>>1;
+        }
+        return ans;
     }
 }
