5f68e0f459979ee94271d425797b0900b53010e8
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-03-12 16:30:46 +0530

    Time: 4 ms (92.73%) | Memory: 47.4 MB (19.61%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,36 +1,30 @@
 class Solution {
-    private boolean isCycle(List<List<Integer>> adj,int[] visited,int[] pathVis,int node)
-    {
-        if(visited[node]==1&&pathVis[node]==1){
-            return true;
+    private boolean dfs(List<List<Integer>> adj , boolean[] pathVisited , boolean[] visited,int node){
+        if(pathVisited[node])return false;
+        if(visited[node])return true;
+        visited[node] = true;
+        pathVisited[node] = true;
+        for(Integer i : adj.get(node)){
+            if(!dfs(adj,pathVisited,visited,i))return false;
         }
-        if(visited[node]==1)return false;
-        visited[node] = 1;
-        pathVis[node] = 1;
-        List<Integer> subAdj = adj.get(node);
-        for(int i=0;i<subAdj.size();i++){
-            if(isCycle(adj,visited,pathVis,subAdj.get(i)))return true;
-        }
-        pathVis[node] = 0;
-
-        return false;
+        pathVisited[node] = false;
+        return true;
     }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList();
-        for(int i=0;i<numCourses;i++){
+        for(int i=0 ; i<numCourses ; i++){
             adj.add(new ArrayList());
         }
-        for(int i=0;i<prerequisites.length;i++){
+        for(int i=0 ; i<prerequisites.length ; i++){
             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
-        int[] visited = new int[numCourses];
-        int[] pathVis = new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            if(visited[i]==0){
-                if(isCycle(adj,visited,pathVis,i))return false;
+        boolean[] visited = new boolean[numCourses];
+        for(int i=0 ; i<numCourses ; i++){
+            if(!visited[i]){
+                if(!dfs(adj,new boolean[numCourses],visited,i))return false;
+
             }
         }
         return true;
-        
     }
 }
