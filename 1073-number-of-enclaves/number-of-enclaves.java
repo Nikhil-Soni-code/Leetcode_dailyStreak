df81f0097fb4748c920f0517da4b7f4f1476cc91
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-03-15 00:23:25 +0530

    Time: 9 ms (52.02%) | Memory: 62 MB (21.65%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,41 +1,30 @@
 class Solution {
-    private void dfs(int[][] grid,int i,int j){
-        if(i<0||j<0||i==grid.length||j==grid[0].length||grid[i][j]==0)return;
+    private int count(int[][] grid,int i,int j){
+        if(i<0 || j<0 || i>= grid.length || j>= grid[0].length || grid[i][j]==0)return 0;
+        if(grid[i][j]==1 && (i==0 || j==0 || i==grid.length-1 || j==grid[0].length-1))return -1;
+
         grid[i][j] = 0;
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid,i,j-1);
-        dfs(grid,i,j+1);
+        int a = count(grid,i-1,j);
+        int b = count(grid,i,j-1);
+        int c = count(grid,i,j+1);
+        int d = count(grid,i+1,j);
 
-
+        if(a==-1 || b==-1 || c==-1 || d==-1)return -1;
+        return 1+a+b+c+d;
 
     }
     public int numEnclaves(int[][] grid) {
-        for(int i=0;i<grid.length;i++){
-            if(grid[i][0]==1){
-                dfs(grid,i,0);
+        int ans = 0;
+        for(int i=0 ; i<grid.length ; i++){
+            for(int j=0 ; j<grid[0].length ; j++){
+                if(grid[i][j]==1){
+                    int co = count(grid,i,j);
+                    if(co!=-1){
+                        ans+=co;
+                    }
+                }
             }
         }
-        for(int i=0;i<grid.length;i++){
-            if(grid[i][grid[0].length-1]==1){
-                dfs(grid,i,grid[0].length-1);
-            }
-        }
-        for(int j=0;j<grid[0].length;j++){
-            if(grid[0][j]==1){
-                dfs(grid,0,j);
-            }
-        }
-        for(int j=0;j<grid[0].length;j++){
-            if(grid[grid.length-1][j]==1){
-                dfs(grid,grid.length-1,j);
-            }
-        }
-        int count = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1)count++;
-            }
-        }return count;
+        return ans;
     }
 }
