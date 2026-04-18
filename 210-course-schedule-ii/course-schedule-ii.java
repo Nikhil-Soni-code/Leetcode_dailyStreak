243f219e532663ce7283863b864f59f874926e5b
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-04-18 13:35:47 +0530

    Time: 5 ms (83.65%) | Memory: 47.8 MB (26.19%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,56 +1,51 @@
 class Solution {
-    int idx = 0;
-    private boolean isCycle(List<List<Integer>> adj,int[] visited,int[] pathVis,int node)
-    {
-        if(visited[node]==1&&pathVis[node]==1){
-            return true;
+    private boolean check(List<List<Integer>> adj , int node , boolean[] visited , boolean[] pathVisited){
+        if(pathVisited[node])return true;
+        if(visited[node])return false;
+        visited[node] = true;
+        pathVisited[node] = true;
+        for(Integer i : adj.get(node)){
+            if(check(adj,i,visited,pathVisited))return true;
         }
-        if(visited[node]==1)return false;
-        visited[node] = 1;
-        pathVis[node] = 1;
-        List<Integer> subAdj = adj.get(node);
-        for(int i=0;i<subAdj.size();i++){
-            if(isCycle(adj,visited,pathVis,subAdj.get(i)))return true;
-        }
-        pathVis[node] = 0;
 
+        pathVisited[node] = false;
         return false;
     }
-    private void topoSort(List<List<Integer>> adj,int node,int[] visited,int[] ans){
-        if(visited[node]==1)return;
-        visited[node] = 1;
-        List<Integer> subAdj = adj.get(node);
-        for(int i=0;i<subAdj.size();i++){
-            topoSort(adj,subAdj.get(i),visited,ans);
+    int idx = 0;
+    private void topoSort(List<List<Integer>> adj , int node , boolean[] visited,int[] ans){
+        if(visited[node])return;
+        visited[node] = true;
+        for(Integer i : adj.get(node)){
+            topoSort(adj,i,visited,ans);
         }
         ans[idx--] = node;
+
+
     }
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList();
-        for(int i=0;i<numCourses;i++){
+        for(int i=0 ; i<numCourses ; i++){
             adj.add(new ArrayList());
         }
-        for(int i=0;i<prerequisites.length;i++){
-            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        for(int[] a : prerequisites){
+            adj.get(a[1]).add(a[0]);
         }
-        int[] visited = new int[numCourses];
-        int[] pathVis = new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            if(visited[i]==0){
-                if(isCycle(adj,visited,pathVis,i))return new int[]{};
-            }
+        boolean[] visited = new boolean[numCourses];
+        Arrays.fill(visited,false);
+        //cycle detection
+        for(int i=0 ; i<numCourses ; i++){
+            if(check(adj,i,visited,new boolean[numCourses]))return new int[]{};
         }
-        Arrays.fill(visited,0);
-        Arrays.fill(pathVis,0);
+        System.out.println("yo");
+        //topoSort
         int[] ans = new int[numCourses];
-        idx = ans.length-1;
-        for(int i=0;i<numCourses;i++){
-            if(visited[i]==0){
-                topoSort(adj,i,visited,ans);
-            }
-        }
-        return ans;
-
+        idx = numCourses-1;
+        Arrays.fill(visited,false);
+        for(int i=0 ; i<numCourses ; i++){
+            if(!visited[i])topoSort(adj,i,visited,ans);
+        }return ans;
         
+        
+
     }
 }
