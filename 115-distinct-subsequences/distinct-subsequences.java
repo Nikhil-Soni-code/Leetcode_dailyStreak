9dd585426befb456ff5cdbaab576ca7df07b97ff
Nikhil Soni <nikhilsoni200605@gmail.com>
2026-09-09 14:01:14 +0530

    Time: 22 ms (23.19%) | Memory: 54.5 MB (23.94%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,21 +1,20 @@
 class Solution {
-    private int find(String s,String t,int i,int j,int[][] dp){
-        if(j==0)return 1;
-        if(i==0)return 0;
+    private int find(String s , String t , int i, int j,int[][] dp){
+        if(j<0)return 1;
+        if(i<0 || j<0)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s.charAt(i-1)==t.charAt(j-1)){
-            dp[i][j] = find(s,t,i-1,j-1,dp)+find(s,t,i-1,j,dp);
-            return dp[i][j];
-        }else{
-            dp[i][j] = find(s,t,i-1,j,dp);
-            return dp[i][j];
+        int ans = 0;
+        if(s.charAt(i)==t.charAt(j)){
+            ans += find(s,t,i-1,j-1,dp);
         }
+        dp[i][j] = ans + find(s,t,i-1,j,dp);
+        return dp[i][j];
     }
     public int numDistinct(String s, String t) {
-        int[][] dp = new int[s.length()+1][t.length()+1];
-        for(int[]a:dp){
+        int[][] dp = new int[s.length()][t.length()];
+        for(int[] a: dp){
             Arrays.fill(a,-1);
         }
-        return find(s,t,s.length(),t.length(),dp);
+        return find(s,t,s.length()-1,t.length()-1,dp);
     }
 }
